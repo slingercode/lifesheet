20947f85580f731147f6aa880f5c0f0543666d03
Noel Espino Córdova <github@ednoesco.com>
2023-10-14 17:45:52 +0000

    Add cookie based auth with JWT (#2)

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -1,13 +1,26 @@
+import { Button } from "~/ui/button";
 import { Link } from "../link";
 import { ThemeSelector } from "./theme-selector";
 
-export const Header = () => {
+type HeaderProps = {
+  session: boolean;
+};
+
+export const Header = ({ session }: HeaderProps) => {
   return (
     <header className="flex items-center justify-between p-5">
-      <nav>
-        <Link href="/" className="text-lg">
+      <nav className="flex items-center gap-5">
+        <Link href="/" className="text-lg font-bold">
           lifesheet
         </Link>
+
+        {session && (
+          <Button variant="ghost" size="sm">
+            <Link href="/capture" className="font-light hover:no-underline">
+              capture
+            </Link>
+          </Button>
+        )}
       </nav>
 
       <ThemeSelector />
